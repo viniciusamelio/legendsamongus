2b8d552482943ab104308b2b838717d39a0ee2a4
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-06-10 21:15:28 -0300

    Adicionando navbar

---
+++ Templates/header.php
@@ -0,0 +1,42 @@
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Roboto|Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Sources/CSS/master.css">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Legends Among Us - E-sports</title>
+    </head>
+    <body style="background: #111019">
+
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark " style="font-family: 'Rubik'">
+            <a class="navbar-brand" href="index.php">
+                <img src="Sources/Images/logo.svg" alt="Logo" height="180px">
+            </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Dropdown" aria-controls="NavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="Dropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Equipe</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Staff</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Wallpapers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+              </li>
+
+            </ul>
+          </div>
+      </nav>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include_once('Templates/header.php');
+?>
+<div class="jumbotron" style="width:100%;height:100vh;background:url('') center center">
+
+</div>
+
+<?php
+include_once('Templates/footer.php');
+?>
