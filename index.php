032e7741aa17cb92b47c72efb4d918ec7c1a5f5a
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-06-11 21:23:28 -0300

    Alterado o banner da index, Iniciada a estrutura de notícias da index.

--- index.php
+++ index.php
@@ -2,20 +2,65 @@
 include_once('Templates/header.php');
 ?>
 
-<div class="jumbotron" style="width:100%;height:100vh;background:url('Sources/Images/Index-Banner.png') no-repeat center center;background-size:cover">
+<section style="width:100%;height:100vh;background:url('Sources/Images/Index-Banner.png') no-repeat center center;background-size:cover;background-color:none;padding-top:50px">
     <div class="container">
         <div class="row" style="height:150px">
 
         </div>
     <div class="row">
         <div class="col-lg-8 col-md-8 col-sm-12 text-left">
-            <h1  class="title text-light">Lendas nunca morrem!</h1>
-            <h2  class="title text-light">Seja parte de nossa história, seja parte da lenda!</h2>
-            <p class="text-light">Bem vindos ao site das lendas! Aqui você poderá se aproximar de nós e ver que nossos jogadores são gente, como a gente. </p>
+            <h1  class="title texto-laranja " >Lendas nunca morrem!</h1>
+            <h2  class="sub-title texto-laranja">Seja parte de nossa história, seja parte da lenda!</h2>
+            <p class="text-white">Bem vindos ao site das lendas! Aqui você poderá se aproximar de nós e ver que nossos jogadores são gente, como a gente. </p>
+
+                <button type="button" class="btn btn-info radiuss" name="twitter"><i class="fab fa-twitter "></i></button>
+
+                <button type="button" class="btn radiuss btn-blue"  name="facebook">.<i class="fab fa-facebook-f"></i></button>
+
+                <button type="button" class="btn radiuss btn-danger"  name="instagram"><i class="fab fa-instagram"></i></button>
+
+
         </div>
+
+
     </div>
     </div>
-</div>
+
+
+</section>
+
+
+    <section class="container" style="margin-top: 30px">
+
+            <div class="col-12 text-center text-secondary">
+                <h1 class="sub-title">Acompanhe-nos</h1>
+            </div>
+            <br>
+            <article class="text-left">
+                <div class="row">
+                    <div class="col-lg-8 col-sm-12 bg-primary" style="padding:10px;min-height:250px" >
+                        <h2>Título da Notícia</h2>
+                        <p>LOREM IPSUM DOLOR SET AMEM DO TRABSON DO DALE DEO DELE</p>
+                    </div>
+
+                    <div class="col-lg-4 col-sm-12 bg-secondary" style="padding:10px;min-height:250px" >
+                        <h2>Título da Notícia</h2>
+                        <p>LOREM IPSUM DOLOR SET AMEM DO TRABSON DO DALE DEO DELE</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 bg-blue" style="padding:10px;min-height:250px" >
+                        <h2>Título da Notícia</h2>
+                        <p>LOREM IPSUM DOLOR SET AMEM DO TRABSON DO DALE DEO DELE</p>
+                    </div>
+                </div>
+            </article>
+
+
+    </section>
+
+
 
 <?php
 include_once('Templates/footer.php');
